29ac95be7e4176a4fbdfecb50c53d37a20782b0b
loickcherimont <loickcherimont@gmail.com>
2024-01-24 06:29:59 +0100

    Allow user to search a new movie

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-'use client' // to use useEffect with Next
+'use client' // to use hooks with Next
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaFilm, FaSearch, FaStar } from "react-icons/fa";
 
 
@@ -10,27 +10,18 @@ export default function App() {
   const API_KEY = "xxx"
 
   // For form
-  const [title, setTitle] = useState("")
-  const [isSearching, setSearching] = useState(false)
+  const [data, setData] = useState({})
 
-  // For found movie
-  // Infos to retrieve on the target movie
-  const [movieTitle, setMovieTitle] = useState("")
-  const [movieWriter, setMovieWriter] = useState("")
-  const [movieImdbRating, setMovieImdbRating] = useState("")
-  const [moviePlot, setMoviePlot] = useState("")
-  const [moviePoster, setMoviePoster] = useState("")
-  const [movieSearched, setMovieSearched] = useState(false) // to display results
+  // Indicator
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
-  const searchMovie = (e) => {
+  let [title, setTitle] = useState("")
+
+  // Search user's movie  
+  const handleSubmit = (e) => {
     e.preventDefault()
-    setSearching(true)
-    e.target.reset()
-  }
+    title = title.toLowerCase()
 
-  useEffect(() => {
-
-    // if (isSearching) {
     fetch(`http://www.omdbapi.com/?t=${title}&apikey=${API_KEY}`)
       .then(response => {
         if (!response.ok) {
@@ -42,22 +33,16 @@ export default function App() {
         return response.json()
 
       })
-      .then(data => {
-        setMovieTitle(data.Title)
-        setMovieWriter(data.Writer)
-        setMovieImdbRating(data.imdbRating)
-        setMoviePlot(data.Plot)
-        setMoviePoster(data.Poster)
-        setMovieSearched(true)
+      .then(d => {
+        setIsSubmitted(true)
+        setData(d)
+        console.log(data)
       })
 
-    return () => {
-      console.log("Clearing")
-    }
-    // }
-
-
-  }, [isSearching])
+    // Reset field and submit indicator 
+    setTitle("")
+    setIsSubmitted(false)
+  }
 
   return <div className="app bg-black text-white min-h-screen max-h-full">
     {/* HEADER */}
@@ -68,10 +53,18 @@ export default function App() {
     {/* HERO */}
     <div>
       {/* Where user searches his movies */}
-      <form className="mb-3 text-center" onSubmit={searchMovie}>
+      <form className="mb-3 text-center" onSubmit={handleSubmit}>
         <label htmlFor="movieInput">Rechercher un film</label>
         <div className="flex justify-center mt-2">
-          <input type="text" placeholder="Aladdin, ..." className="placeholder:italic rounded-l text-orange-500 bg-transparent border border-orange-500 px-2" name="movie_input" id="movieInput" onChange={(e) => setTitle(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Aladdin, ..."
+            className="placeholder:italic rounded-l text-orange-500 bg-transparent border border-orange-500 px-2"
+            name="movie_input"
+            id="movieInput"
+            onChange={e => setTitle(e.target.value)}
+            value={title}
+          />
           <button
             type="submit"
             className="bg-orange-500 rounded-r text-white h-auto p-2">
@@ -81,30 +74,28 @@ export default function App() {
 
       </form>
 
+
       <div className="p-2">
         <h2 className="text-orange-300 text-xl">Résultats</h2>
         <p className="text-slate-300 italic text-sm">Vos films apparaîtront ici...</p>
-        {/* Where results for the query appear */}
-        {/* Display only one column on mobile */}
 
-        <div className="results grid grid-cols-1 place-items-center gap-y-6 p-3">
-          {/* Todo : Do a conditional rendering
-            before that separe form from page.js in a new /components folder
-          */}
-          {movieSearched && <div className="movie-1 flex justify-center gap-3 w-[400px]">
+
+        {isSubmitted && <div className="results grid grid-cols-1 place-items-center gap-y-6 p-3">
+
+          <div className="movie-1 flex justify-center gap-3 w-[400px]">
             <div className="movie-image w-[200px] h-[300px] rounded overflow-hidden">
-              <img src={moviePoster} alt="First cover of movie" className="w-full h-full" />
+              <img src={data.Poster} alt="First cover of movie" className="w-full h-full" />
             </div>
             <div className="movie-infos w-[200px] h-[300px] flex flex-col justify-between items-center p-2">
-              <h2 className="font-bold text-white">{movieTitle}</h2>
-              <p className="italic text-slate-500">{movieWriter}</p>
-              <div className="text-yellow-500 inline-flex justify-center items-center gap-2">{movieImdbRating} <FaStar /></div>
-              <p className="text-slate-300 text-xs text-justify">{moviePlot}</p>
+              <h2 className="font-bold text-white">{data.Title}</h2>
+              <p className="italic text-slate-500">{data.Writer}</p>
+              <div className="text-yellow-500 inline-flex justify-center items-center gap-2">{data.imdbRating} <FaStar /></div>
+              <p className="text-slate-300 text-xs text-justify">{data.Plot}</p>
 
             </div>
-          </div>}
+          </div>
+        </div>}
 
-        </div>
       </div>
     </div >
 
